126d3b32fa1e25d7530c546454f80ddee364ec38
Jufralice <france.julien@gmail.com>
2023-02-07 21:25:09 +0100

    initial commit?

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 see example here:
 https://github.com/b0o/surfingkeys-conf#installation
+
+path to your config file
+file:///Users/julien/Projects/personal/surfingkeys/surfingkeys.js
---
+++ surfingkeys.js
@@ -0,0 +1,12 @@
+// path to your config file (add that to the 'Load settings from' of the surfingkeys configuration (when you hit ;e)
+// file:///Users/julien/Projects/personal/surfingkeys/surfingkeys.js
+// API doc:
+// https://github.com/brookhong/Surfingkeys/blob/master/docs/API.md#hintsstyle
+
+/* api.Hints.setCharacters("fjdkaslweruio"); */
+api.Hints.setCharacters("fjdkslawueiro");
+api.Hints.style(
+  "font-size: 13px; border: solid 3px #552a48; color:#efe1eb; background: none; background-color: #552a48;"
+);
+
+api.Visual.style("marks", "font-size: 23px;");
